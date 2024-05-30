cb276326711ec9884022a28d68dbba08feeb8a5b
scnkera <149810869+scnkera@users.noreply.github.com>
2024-05-30 15:10:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const buttonClicked = (event) => {
+    console.log("Button clicked");
+    const screen = document.getElementById("screen");
+    
+    // console.log(event);
+    const numberClicked = event.target.innerHTML;
+
+    if (screen.innerHTML === "0"){
+        screen.innerHTML = numberClicked;
+    } else {
+        screen.innerHTML += numberClicked;
+    }
+};
+
+const clearButton = () => {
+    const scren = document.getElementById("screen");
+    screen.innerHTML = "0"l
+};
+
+const equalClicked = () => {
+    console.log("equal clicked");
+    const screen = document.getElementById("screen");
+    try{
+        const result = eval(screen.innerHTML);
+        console.log(result);
+        screen.innerHTML = result;
+    } catch (error) {
+        screen.innerHTML = "Error";
+    }
+
+};
+
+const allButtons = document.getElementsByTagName("button")
+for (const individualButton of allButtons){
+
+    if (individualButton.innerHTML === "="){
+        individualButton.onclick = equalClicked;
+    } else if (individualButton.innerHTML === "c"){
+        individualButton.onclick = clearButton;
+    } else {
+        individualButton.onclick = buttonClicked;
+    }
+
+};
